8ef4affd6bbbcbf1a128fe314757adf080d0d122
JuanAntonaccio <jmantonaccio@gmail.com>
2021-08-26 01:07:35 +0000

    ejer 7.2 finish

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,6 +1,16 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 let counts = {};
 
-// your code here
+par=par.toLowerCase();
+for (let i=0;i<par.length;i++){
+    if(par[i] != " " ){
+        if(counts[par[i]]==undefined){ // par[i] in counts
+           counts[par[i]]=1;
+        }else {
+            counts[par[i]]=counts[par[i]]+1;
+        }
+        
+    }
 
+}
 console.log(counts);
