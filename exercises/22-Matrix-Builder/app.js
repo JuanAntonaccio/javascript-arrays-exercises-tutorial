43678b8d8fb13e0214eeed55382482054cba3ec5
JuanAntonaccio <jmantonaccio@gmail.com>
2021-09-02 09:39:04 +0000

    Hasta ejercicio 23

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,9 +1,12 @@
 // Code goes here
 function matrixBuilder(number){
-   let mat=[],[];
+   let mat= new Array(number);
+   for(let i=0;i<number;i++){
+       mat[i]= new Array(number);
+   }
    for(let i=0;i<number;i++){
        for(let j=0;j<number;j++){
-           mat[i][j]=(Math.floor(Math.random()*1));
+           mat[i][j]=(Math.floor(Math.random()*2));
        }
    }
    return mat;
--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -3,4 +3,30 @@ let parking_state = [
   [0,0,0],
   [1,1,2]
 ]
+
+function getParkingLotState(matriz){
+  let mdisp=0;
+  let mocu=0;
+  for(let i=0;i<matriz.length;i++){
+    for(let j=0;j<matriz[i].length;j++){
+      if (matriz[i][j]==1){
+        mocu++;
+      }
+      if (matriz[i][j]==2){
+        mdisp++;
+      } 
+      
+
+    }
+    
+  }
+  let mtotal=mocu+mdisp;
+    const state = {
+      totalSlots: mtotal,
+      availableSlots: mdisp,
+      occupiedSlots: mocu
+    }
+    
+    return state;
+}
 console.log(getParkingLotState(parking_state))
