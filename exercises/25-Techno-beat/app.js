6f560d2d79f8fa992cd7b79f17484e9a9f38f61e
JuanAntonaccio <jmantonaccio@gmail.com>
2021-09-02 11:04:14 +0000

    25 error de testeo

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,5 +1,22 @@
 
 // test Data
+function lyricsGenerator(vector){
+    let mcont=0;
+    let text="";
+    for(let i=0;i<vector.length;i++){
+       if (vector[i]==0){
+           text+="Boom ";
+       } else {
+           text+="Drop the base ";
+           mcont++;
+           if (mcont==3){
+               mcont=0;
+               text+="!!!Break the base!!!";
+           }
+       }
+    }
+    return text;
+}
 console.log(lyricsGenerator([0,0,1,1,0,0,0]))
 console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
 console.log(lyricsGenerator([0,0,0]))
