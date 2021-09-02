e1178cabaaf102ff9a85ce5cf5d580a463a7eb17
JuanAntonaccio <jmantonaccio@gmail.com>
2021-09-02 10:47:00 +0000

    24 costo pero salio

--- exercises/24-Making-a-ul/app.js
+++ exercises/24-Making-a-ul/app.js
@@ -8,19 +8,26 @@ let allColors = [
 	{label: 'Purple', sexy: false},
 ];
 
+
 function generateLI(color){
 	// your code here
+	let text="";
+	text="<li>"+color.label+"</li>";
+    return text;
 }
 
 function filterColors(color){
 	// your code here
+    return (color.sexy==true);
+    
 }
 
 function generateHTMLFromArray(array){
 	
 	let filteredOptions = array.filter(filterColors);
+	console.log(filteredOptions);
 	let LIs = filteredOptions.map(generateLI);
-
+    console.log(LIs);
 	let htmlString = '<ul>';
 	LIs.forEach(function(elm){
 		htmlString += elm;
